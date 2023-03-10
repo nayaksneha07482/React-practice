9a0facfb9e48f19af6c0ddf4fff18d907fb8c70b
nayaksneha07482 <nayaksneha07482@gmail.com>
2023-03-10 18:08:06 +0530

    axios

--- Axios/ch1/src/App.js
+++ Axios/ch1/src/App.js
@@ -1,12 +1,50 @@
 
 import './App.css';
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [error,setError] = useState("")
+
+  // using promise
+
+  // useEffect(() => {
+  //   axios
+  //       .get("https://jsonplaceholder.typicode.com/posts")
+  //       .then((res)=>setData(res.data))
+  //       .catch((err)=> setError(err.message))
+  // }, [])
+  
+  // using async await
+      const getApiData = async () => {
+        try {
+          const res = await axios.get("https://jsonplaceholder.typicode.com/posts")
+          setData(res.data);
+        } catch (err) {
+          setError(err.message)
+        }
+      }
+  useEffect(()=>{
+    getApiData()
+  },[])
+
+  
   return (
     <>
-      
+      <h1>Axios concept in React</h1>
+      {/* error handling */}
+      {error !== "" && <h2>{error}</h2>}
+      <div className='grid'>
+      {data.map((post)=>{
+          const {id, title, body} = post;
+
+          return <div className="card" key={id}>
+            <h2>{title.slice(0,15).toUpperCase()}</h2>
+            <p>{body.slice(0,100)}</p>
+          </div>
+      })}
+      </div>
     </>
   );
 }
