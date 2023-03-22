4ac037d162a04ef765dcdf98d918ad9c64bea6d9
nayaksneha07482 <nayaksneha07482@gmail.com>
2023-03-22 17:53:30 +0530

    closure

--- 1. Js Practice/Callback,async,await/index.js
+++ 1. Js Practice/Callback,async,await/index.js
@@ -1,12 +1,11 @@
-let arr1 =[{name:"ram", age:"20"},
-            {name: "syam", age:"30"}]
+let arr1 = [{ name: "ram", age: "20" },
+{ name: "syam", age: "30" }]
 
-    function data(){
-        setTimeout(()=>{
-         console.log("ashok")   
-        },2000)
-    }
-    data();
+function data() {
+    setTimeout(() => {
+        console.log("ashok")
+    }, 2000)
+}
+data();
 
 
-  
