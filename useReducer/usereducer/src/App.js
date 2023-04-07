4ad0cf233c78e579b3b38c4469d81a899d4141b5
nayaksneha07482 <nayaksneha07482@gmail.com>
2023-04-07 12:04:02 +0530

    usereducer

---
+++ useReducer/usereducer/src/App.js
@@ -0,0 +1,26 @@
+import React, { useReducer } from 'react';
+
+const initialState = { count: 0 };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 };
+    case 'decrement':
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
+  }
+}
+
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div>
+      Count: {state.count}
+      <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+    </div>
+  );
+}
